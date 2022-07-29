4b1d1031122f69289d2cd1cab2c26b924951de36
김안나 <yorocobe@gmail.com>
2022-07-29 19:08:31 +0900

    chore: 로그인 페이지 로고 추가

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../../_actions/user_action";
 import { useNavigate } from "react-router-dom";
+import Logo from "../../../utils/imgs/tasking_logo.png";
 import "tailwindcss/tailwind.css";
 
 const LoginPage = () => {
@@ -37,49 +38,52 @@ const LoginPage = () => {
   };
 
   return (
-    <div class="flex justify-center items-center w-full h-screen">
-      <form class="flex flex-col w-72" onSubmit={onSubmitHandler}>
-        <div>
+    <div className="flex justify-center items-center w-full h-screen">
+      <form className="flex flex-col w-72" onSubmit={onSubmitHandler}>
+        <div className="w-full">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="mt-4">
           <label>
-            <span class="text-sm font-medium text-slate-700">ID</span>
+            <span className="text-sm font-medium text-slate-700">ID</span>
             <input
               type="text"
               value={Id}
               onChange={onIdHandler}
-              class="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+              className="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
             />
           </label>
         </div>
         <div>
           <label>
-            <span class="text-sm font-medium text-slate-700">Password</span>
+            <span className="text-sm font-medium text-slate-700">Password</span>
             <input
               type="password"
               value={Password}
               onChange={onPasswordHandler}
-              class="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              className="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
             />
           </label>
         </div>
         <button
           type="submit"
-          class="mt-2 py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-600 hover:bg-green-800"
+          className="mt-4 py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-600 hover:bg-green-800"
         >
           확인
         </button>
 
-        <button class="mt-8 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-blue-500 hover:text-white">
+        <button className="mt-8 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-blue-500 hover:text-white">
           google 로그인
         </button>
-        <button class="mt-2 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-slate-800 hover:text-white">
+        <button className="mt-2 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-slate-800 hover:text-white">
           GitHub 로그인
         </button>
-        <button class="mt-2 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-blue-900 hover:text-white">
+        <button className="mt-2 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-blue-900 hover:text-white">
           facebook 로그인
         </button>
-        <button class="mt-2 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-yellow-300 ">
+        <button className="mt-2 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-yellow-300 ">
           kakao 로그인
         </button>
       </form>
