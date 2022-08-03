09d56b0f1a41457147d829ae6f58f2deccf8b9aa
김안나 <yorocobe@gmail.com>
2022-08-03 16:43:50 +0900

    chore: 로그인 페이지 레이아웃 디자인 변경 및 반응형 구현

--- client/src/components/views/LoginPage/LoginPage.jsx
+++ client/src/components/views/LoginPage/LoginPage.jsx
@@ -1,92 +1,73 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { loginUser } from "../../../_actions/user_action";
-import { useNavigate } from "react-router-dom";
-import Logo from "../../../utils/imgs/tasking_logo.png";
-import "tailwindcss/tailwind.css";
+import React from "react";
+import logoImage from "../../../utils/images/tasking_logo.png";
+import { ReactComponent as KakaoLogoImage } from "../../../utils/images/kakaotalk.svg";
+import { faFacebook, faGithub, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const LoginPage = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const [Id, setId] = useState("");
-  const [Password, setPassword] = useState("");
-
-  const onIdHandler = (event) => {
-    setId(event.currentTarget.value);
-  };
-
-  const onPasswordHandler = (event) => {
-    setPassword(event.currentTarget.value);
-  };
-
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
-    console.log(Id, Password);
-
-    let body = {
-      id: Id,
-      password: Password,
-    };
-
-    dispatch(loginUser(body)).then((response) => {
-      if (response.payload.loginSuccess) {
-        navigate("/");
-      } else {
-        alert("Error");
-      }
-    });
-  };
-
   return (
     <div className="flex justify-center items-center w-full h-screen">
-      <form className="flex flex-col w-72" onSubmit={onSubmitHandler}>
-        <div className="w-full">
-          <img src={Logo} alt="logo" />
-        </div>
-        <div className="mt-4">
-          <label>
-            <span className="text-sm font-medium text-slate-700">ID</span>
+      <main className="w-10/12 sm:w-10/12 md:w-4/6 lg:w-1/3 p-10 lg:p-20 rounded-lg border border-zinc-200  dark:bg-white">
+        <form className="flex flex-col">
+          <img src={logoImage} alt="task-ing logo" className="w-32 sm:w-32 md:w-48 lg:w-60 m-auto" />
+
+          <h2 className="mt-6 sm:mt-6 md:mt-8 text-center text-zinc-800 text-sm sm:text-sm md:text-md lg:text-lg">
+            Sign in to Task-ing.
+          </h2>
+
+          <label htmlFor="id" className="flex flex-col sm:flex-col mt-6 sm:mt-6 md:flex-row md:items-center md:mt-10">
+            <span className="text-zinc-800 text-sm sm:text-sm md:text-md md:w-24">ID</span>
             <input
               type="text"
-              value={Id}
-              onChange={onIdHandler}
-              className="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+              placeholder="Input Your ID"
+              id="id"
+              className="mt-2 text-zinc-800 input input-bordered w-full max-w-sm bg-white hover:border-green-600 focus:border-green-900"
             />
           </label>
-        </div>
-        <div>
-          <label>
-            <span className="text-sm font-medium text-slate-700">Password</span>
+
+          <label htmlFor="password" className="flex flex-col sm:flex-col mt-6 md:flex-row md:items-center">
+            <span className="text-zinc-800 text-sm sm:text-sm md:text-md md:w-24">password</span>
             <input
               type="password"
-              value={Password}
-              onChange={onPasswordHandler}
-              className="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+              placeholder="Input Your Password"
+              id="password"
+              className="mt-2 text-zinc-800 input input-bordered w-full max-w-sm bg-white hover:border-green-600 focus:border-green-900"
             />
           </label>
-        </div>
-        <button
-          type="submit"
-          className="mt-4 py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-600 hover:bg-green-800"
-        >
-          확인
-        </button>
-      </form>
 
-      <button className="mt-8 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-blue-500 hover:text-white">
-        google 로그인
-      </button>
-      <button className="mt-2 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-slate-800 hover:text-white">
-        GitHub 로그인
-      </button>
-      <button className="mt-2 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-blue-900 hover:text-white">
-        facebook 로그인
-      </button>
-      <button className="mt-2 py-2 px-4 font-semibold rounded-lg shadow-md text-slate-700 bg-slate-50 hover:bg-yellow-300 ">
-        kakao 로그인
-      </button>
+          <button className="mt-6 sm:mt-6 md:mt-8 btn text-white border-none bg-green-600 hover:bg-green-700 focus:bg-green-900">
+            로그인
+          </button>
+          <div className="flex flex-col items-start md:flex-row md:justify-between md:items-center mt-2">
+            <label className="label cursor-pointer justify-start">
+              <input type="checkbox" className="checkbox checkbox-xs" />
+              <span className="label-text ml-2 text-slate-400">Remember me</span>
+            </label>
+            <span className="text-sm text-slate-400">forgot ID or Password?</span>
+          </div>
+        </form>
+
+        {/* 소셜 로그인 */}
+        <div className="mt-6 sm:mt-6 md:mt-8 border-solid border-t border-t-zinc-300">
+          <h3 className="mt-4 sm:mt-4 md:mt-6 text-center text-zinc-700 text-sm sm:text-sm md:text-md">
+            다른 방식으로 로그인
+          </h3>
+          <div className="flex mt-3 sm:mt-3 md:mt-8 px-4 sm:px-4 justify-between sm:justify-between md:justify-center">
+            <button className="py-2 px-3 rounded-lg bg-kakao-100 hover:bg-kakao-200 active:bg-kakao-100">
+              <KakaoLogoImage width="16" height="16" fill="white" />
+            </button>
+            <button className="py-2 px-3 md:ml-4 rounded-lg bg-google-200 hover:bg-google-100 active:bg-google-200">
+              <FontAwesomeIcon icon={faGoogle} color="white" />
+            </button>
+            <button className="py-2 px-3 md:ml-4 rounded-lg bg-github-200 hover:bg-github-100 active:bg-github-200">
+              <FontAwesomeIcon icon={faGithub} color="white" />
+            </button>
+            <button className="py-2 px-3 md:ml-4 rounded-lg bg-facebook-200 hover:bg-facebook-100 active:bg-facebook-200">
+              <FontAwesomeIcon icon={faFacebook} color="white" />
+            </button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
