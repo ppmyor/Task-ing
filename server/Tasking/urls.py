50dfa1afdc8fe74014420d072c1c85dc262ea0f4
heyoni <74006293+heyoni@users.noreply.github.com>
2022-08-13 02:28:51 +0900

    fix: 깃 에러 해결

--- server/Tasking/urls.py
+++ server/Tasking/urls.py
@@ -11,35 +11,32 @@ from drf_yasg import openapi
 
 api_patterns = [
     path('accounts/', include('dj_rest_auth.urls')),
-    path('work/', include('works.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('work/',include('works.urls')),
+
 ]
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(api_patterns)),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    schema_view = get_schema_view(
-        openapi.Info(
-            title="Snippets API",
-            default_version='v1',
-            description="Test description",
-            terms_of_service="https://www.google.com/policies/terms/",
-            contact=openapi.Contact(email="contact@snippets.local"),
-            license=openapi.License(name="BSD License"),
-        ),
-        public=True,
-        permission_classes=(permissions.AllowAny,),
-    )
-
-    urlpatterns += [
-        url(r'^swagger(?P<format>\.json|\.yaml)$',
+    path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),        
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
-        url(r'^swagger/$', schema_view.with_ui('swagger',
-                                               cache_timeout=0), name='schema-swagger-ui'),
-        url(r'^redoc/$', schema_view.with_ui('redoc',
-                                             cache_timeout=0), name='schema-redoc'),
-    ]
-
+    url(r'^swagger/$', schema_view.with_ui('swagger',
+                                            cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc',
+                                            cache_timeout=0), name='schema-redoc'),
+] 
