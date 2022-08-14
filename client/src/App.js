d63f3695a32928ff3fca129079dd2de7674b47ea
김안나 <yorocobe@gmail.com>
2022-08-14 17:34:13 +0900

    feat: 구글 소셜 로그인 구현 및 네이버 로고 svg 파일 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "../src/components/views/LoginPage/LoginPage";
+import MainPage from "../src/components/views/MainPage/MainPage";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <Router>
+      <div className="bg-white dark:bg-zinc-800">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
