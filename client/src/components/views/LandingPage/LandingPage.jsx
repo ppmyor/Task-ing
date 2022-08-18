516b60ca77c7098fac804e67457950470ebd8f06
김안나 <yorocobe@gmail.com>
2022-08-19 04:57:53 +0900

    chore: 카카오 소셜로그인 관련 코드 제거

--- client/src/components/views/LandingPage/LandingPage.jsx
+++ client/src/components/views/LandingPage/LandingPage.jsx
@@ -1,33 +1,6 @@
-import axios from "axios";
 import React from "react";
-import { useEffect } from "react";
-import { DOMAIN } from "../../../config/domain";
-import { useNavigate } from "react-router-dom";
+
 const LandingPage = () => {
-  const navigate = useNavigate();
-
-  const request = async () => {
-    let body = {
-      access_token: localStorage.getItem("accessToken"),
-    };
-
-    await axios
-      .post(`${DOMAIN}/accounts/rest-auth/kakao/`, body, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) => {
-        console.log(response);
-        navigate("/");
-      })
-      .catch((error) => console.log(error));
-  };
-
-  useEffect(() => {
-    request();
-  }, []);
-
   return <div>LandingPage</div>;
 };
 
